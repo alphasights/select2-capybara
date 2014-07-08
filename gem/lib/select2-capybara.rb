46cb314302fdc99c5828d7ff7b8011d0bf489538
David Griffiths <david.griffiths@alphasights.com>
2014-07-08 15:06:58 +0100

    Port the helper method

---
+++ gem/lib/select2-capybara.rb
@@ -0,0 +1,16 @@
+module Capybara
+  module Select2
+    def select2(value, options = {})
+      page.find("#s2id_#{options[:from]}").click
+
+      if options.fetch(:multiple, false)
+        find(:xpath, "//body").find(".select2-container-multi .select2-choices .select2-search-field input").set(value)
+      else
+        find(:xpath, "//body").find(".select2-with-searchbox input.select2-input").set(value)
+      end
+
+      page.execute_script(%|$("input.select2-input:visible").keyup();|)
+      find(:xpath, "//body").find(".select2-results li", text: value).click
+    end
+  end
+end
